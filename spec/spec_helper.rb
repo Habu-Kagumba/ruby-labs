0894843e3153a38d9f8726b3bef979043cb4a488
Herbert Kagumba <habukagumba@gmail.com>
2016-02-28 09:26:49 +0300

    (Testing) Add tests Add tests for binary search

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require_relative '../labs/caesar-cipher'
 require_relative '../labs/enumerable'
+require_relative '../labs/binary_search'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
